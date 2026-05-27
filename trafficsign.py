46fde4e044cffe9c0b2514a8f4d873e28dfd1e21
vuongphuc344 <vuongphuc344@gmail.com>
2026-05-27 22:11:19 +0700

    feat: Hoan thien ham get_modle - kien truc CNN 2 block

--- trafficsign.py
+++ trafficsign.py
@@ -111,19 +111,26 @@ def get_model():
     model = tf.keras.models.Sequential([
 
         # Convolutional layers and Max-pooling layers
-        
+        tf.keras.layers.Conv2D(32, (3,3), activation="relu",input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Conv2D(64, (3,3), activation="relu", padding="same"),
+        tf.keras.layers.Conv2D(64, (3,3), activation="relu", padding="same"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         # Flatten units
-        
+        tf.keras.layers.Flatten(),
 
         # Hidden Layers
-        
+        tf.keras.layers.Dense(256, activation="relu"),
+        tf.keras.layers.Dense(128, activation="relu"),
         
         # Dropout
-        
-
+        tf.keras.layers.Dropout(0.5),
               
         # Output layer with output units for all digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
         
     ])
 
